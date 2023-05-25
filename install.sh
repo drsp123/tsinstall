15c1c4e18c556221528e48b45e06a182d4ce53e0
drsp123 <27956273+drsp123@users.noreply.github.com>
2023-05-25 13:04:36 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install curl -y
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+sudo apt update
+sudo upt upgrade -y
+sudo apt-get install tailscale
+tailscale up
+tailscale ip -4
